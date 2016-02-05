e967bfa25520db31cc959406339ad9a9b13a194d
nikkall <niko.kallio@codemate.com>
2016-02-05 15:11:22 +0700

    initialize

---
+++ README.md
@@ -0,0 +1,11 @@
+# github-notetaker-egghead
+
+[Build Your First React App](https://egghead.io/series/build-your-first-react-js-application) is an egghead lesson series.
+
+### Instructions
+
+```bash
+npm install
+webpack -w
+```
+# react-github-notetaker
---
+++ app/config/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../components/Main';
+import Home from '../components/Home';
+import Profile from '../components/Profile';
+import Router from 'react-router';
+import { Route, IndexRoute } from 'react-router';
+
+export default (
+  <Route path="/" component={Main}>
+    <Route path="profile/:username" component={Profile} />
+    <IndexRoute component={Home} />
+  </Route>
+);
